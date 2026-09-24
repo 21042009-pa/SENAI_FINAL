b7fa13fac1d645382dcb396109f51722f63530cf
21042009-pa <paula.gianotto@aluno.senai.br>
2026-09-24 08:19:54 -0300

    documwntação atividade

---
+++ back end/livraria_ebooks/src/swagger.js
@@ -0,0 +1,20 @@
+const swaggerAutogen = require('swagger-autogen')()
+
+const doc = {
+    info:{
+        title:"API Livraria",
+        description: 'Documentação automática da API Livraria utilizando Swagger Autogen',
+        version: '1.0.0'
+    },
+    host: 'localhost:3000',
+    schemes: ['http'],
+
+    
+}
+
+const outputFile =  './swagger_output.json'
+const endpointsFiles = ['./src/routes/index.js']
+
+swaggerAutogen(outputFile, endpointsFiles, doc).then(() => {
+    console.log("Documentação do swagger gerada com sucesso!")
+})
